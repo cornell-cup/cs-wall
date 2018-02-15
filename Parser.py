971af4169844c7d5e0c3ce3363e3309e64225b6d
EageRAssassin <rt389@cornell.edu>
2018-02-15 18:20:13 -0500

    parser updated

--- Parser.py
+++ Parser.py
@@ -1,10 +1,93 @@
+# main function that receives the string of inputCode to output minibot movement
 def runCode(inputCode):
-    # split the text
-    words = inputCode.split()
+    # split the code by lines
+    words = inputCode.split("\n")
 
     # for each word in the line:
     for word in words:
         # print the word
         print(word)
 
-runCode("aaa\n bbb")
+    from collections import defaultdict
+    VariableMap = defaultdict(list)
+    VariableMap['a'].append(1)
+    VariableMap['b'].append(2)
+    VariableMap['c'].append(3)
+
+# function that receives the string of value to output value based on the variable map
+def parseValue(s):
+#     int left, right;
+#     if (s.contains("+")) {
+#     String[] temp = s.split("\\+");
+#     if (VariableMap.containsKey(temp[0])) {
+#     left = VariableMap.get(temp[0]);
+#     } else {
+#     left = Integer.parseInt(temp[0]);
+#     }
+#     if (VariableMap.containsKey(temp[1])) {
+#     right = VariableMap.get(temp[1]);
+#     } else {
+#     right = Integer.parseInt(temp[1]);
+#     }
+#     return left + right;
+#     } else if (s.contains("-")) {
+#     String[]
+#     temp = s.split("\\-");
+#     if (VariableMap.containsKey(temp[0])) {
+#     left = VariableMap.get(temp[0]);
+#     } else {
+#     left = Integer.parseInt(temp[0]);
+#     }
+#     if (VariableMap.containsKey(temp[1])) {
+#     right = VariableMap.get(temp[1]);
+#     } else {
+#     right = Integer.parseInt(temp[1]);
+#     }
+#     return left - right;
+#
+# } else if (s.contains("*")) {
+# String[]
+# temp = s.split("\\*");
+# if (VariableMap.containsKey(temp[0])) {
+# left = VariableMap.get(temp[0]);
+# } else {
+# left = Integer.parseInt(temp[0]);
+# }
+# if (VariableMap.containsKey(temp[1])) {
+# right = VariableMap.get(temp[1]);
+# } else {
+# right = Integer.parseInt(temp[1]);
+# }
+# return left * right;
+# } else if (s.contains("/")) {
+# String[]
+# temp = s.split("\\/");
+# if (VariableMap.containsKey(temp[0])) {
+# left = VariableMap.get(temp[0]);
+# } else {
+# left = Integer.parseInt(temp[0]);
+# }
+# if (VariableMap.containsKey(temp[1])) {
+# right = VariableMap.get(temp[1]);
+# } else {
+# right = Integer.parseInt(temp[1]);
+# }
+# return left / right;
+# }
+# // only
+# one
+# symbol or value
+# if (VariableMap.containsKey(s))
+# {
+# return VariableMap.get(s);
+# } else {
+# return Integer.parseInt(s);
+# }
+    return 0;
+
+# function that receives the string of code to output minibot movement
+# the input in the code contains Forward, Backward, TurnLeft, TurnRight
+def moveRobot(code):
+    return 0;
+
+runCode("Forward\nForward\nForward")
