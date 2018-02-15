7ed26766817bb2c22c82b0b7812252bf0219ce35
EageRAssassin <rt389@cornell.edu>
2018-02-15 18:01:36 -0500

    initial setup

---
+++ Parser.py
@@ -0,0 +1,10 @@
+def runCode(inputCode):
+    # split the text
+    words = inputCode.split()
+
+    # for each word in the line:
+    for word in words:
+        # print the word
+        print(word)
+
+runCode("aaa\n bbb")
