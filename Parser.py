c5a33c783803729c0ce657c2e49b0a7c32f48e00
EageRAssassin <rt389@cornell.edu>
2018-02-24 14:12:00 -0500

    parser completely done

--- Parser.py
+++ Parser.py
@@ -1,3 +1,11 @@
+from collections import defaultdict
+global VariableMap
+VariableMap = defaultdict(list)
+global robotX
+robotX = 0
+global robotY
+robotY = 0
+
 # function to translate the RFID file to the blocks
 def translateRFID(rfidfile):
     file1 = open(rfidfile, "r")
@@ -13,65 +21,66 @@ def translateRFID(rfidfile):
     rfids = file1.readline()
     while rfids:
         rfids = rfids.replace("\n", "")
-        result += (blockmap.get(rfids) + "\n")
+        if blockmap.get(rfids) in ['FOR', 'SET', 'IF', 'WHILE', 'END', 'Forward', 'Backward', 'TurnLeft', 'TurnRight']:
+            result += "\n"
+        if blockmap.get(rfids) in ['DO']:
+            result += " "
+        result += blockmap.get(rfids)
+        if blockmap.get(rfids) in ['FOR', 'SET', 'IF', 'WHILE']:
+            result += " "
         rfids = file1.readline()
     file1.close()
-    # ask: what does this method return? A string block?
     return result
 
 
-
-# Note: the field variablemap should be changed to whatever the field is named in parser
-VariableMap = {}
-
 # function that receives the string of value to output value based on the variable map
 def parseValue(s):
-    left, right = 0;
+    global VariableMap
     if "+" in s:
-        temp = s.split("\\+")
-        if temp[0] in VariableMap.keys():
+        temp = s.split("+")
+        if temp[0] in VariableMap:
             left = VariableMap.get(temp[0])
         else:
             left = int(temp[0])
-        if temp[1] in VariableMap.keys():
+        if temp[1] in VariableMap:
             right = VariableMap.get(temp[1])
         else:
             right = int(temp[1])
         return left+right
     elif "-" in s:
-        temp = s.split("\\-")
-        if temp[0] in VariableMap.keys():
+        temp = s.split("-")
+        if temp[0] in VariableMap:
             left = VariableMap.get(temp[0])
         else:
             left = int(temp[0])
-        if temp[1] in VariableMap.keys():
+        if temp[1] in VariableMap:
             right = VariableMap.get(temp[1])
         else:
             right = int(temp[1])
         return left - right
     elif "*" in s:
-        temp = s.split("\\*")
-        if temp[0] in VariableMap.keys():
+        temp = s.split("*")
+        if temp[0] in VariableMap:
             left = VariableMap.get(temp[0])
         else:
             left = int(temp[0])
-        if temp[1] in VariableMap.keys():
+        if temp[1] in VariableMap:
             right = VariableMap.get(temp[1])
         else:
             right = int(temp[1])
         return left * right
     elif "/" in s:
-        temp = s.split("\\/")
-        if temp[0] in VariableMap.keys():
+        temp = s.split("/")
+        if temp[0] in VariableMap:
             left = VariableMap.get(temp[0])
         else:
             left = int(temp[0])
-        if temp[1] in VariableMap.keys():
+        if temp[1] in VariableMap:
             right = VariableMap.get(temp[1])
         else:
             right = int(temp[1])
         return left / right
-    if s in VariableMap.keys():
+    if s in VariableMap:
         return VariableMap.get(s)
     else:
         return int(s)
@@ -82,12 +91,7 @@ def runCode(inputCode):
     # split the code by lines
     codeLines = inputCode.split("\n")
     movement = ['Forward', 'Backward', 'TurnLeft', 'TurnRight']
-
-    from collections import defaultdict
-    VariableMap = defaultdict(list)
-    VariableMap['a'].append(1)
-    VariableMap['b'].append(2)
-    VariableMap['c'].append(3)
+    global VariableMap
 
     while len(codeLines) > 0:
         code = codeLines.pop(0)
@@ -96,54 +100,51 @@ def runCode(inputCode):
             continue
         if code.split(" ")[0] == "FOR":
             ForCode = ""
-            temp = codeLines.remove(0)
+            temp = codeLines.pop(0)
             while temp != "END":
                 ForCode += temp + "\n"
-                temp = codeLines.remove(0)
+                temp = codeLines.pop(0)
             loopNum = int(code.split(" ")[1].split("x")[0]);
             for i in range(0, loopNum) :
                 runCode(ForCode)
             continue
         if code.split(" ")[0] == "SET":
-            # TODO
-            VariableMap[code.substring(4, code.length()).split("=")[0].replace(" ", "")].append(
-                            parseValue(code.split("=")[1].replace(" ", "")))
+            one, two = code[4:].split("=")
+            VariableMap[one.replace(" ", "")] = parseValue(two.replace(" ", ""))
             continue
         if code.split(" ")[0] == "IF":
-            logic = code.substring(3, code.length()).replace(" ", "");
+            logic = code[3:code.find("DO")].replace(" ", "")
             IfCode = ""
-            temp = codeLines.remove(0)
+            temp = codeLines.pop(0)
             while temp != "END":
-                IfCode += temp + "\n";
-                temp = codeLines.remove(0);
+                IfCode += temp + "\n"
+                temp = codeLines.pop(0)
             if parseLogic(logic):
-                runCode(IfCode);
-            continue;
+                runCode(IfCode)
+            continue
         if code.split(" ")[0] == "WHILE":
-            logic = code.substring(6, code.indexOf("DO")).replace(" ", "");
-            WhileCode = "";
-            temp = codeLines.remove(0);
-            while temp!= "END":
-                WhileCode += temp + "\n";
-                temp = codeLines.remove(0);
+            logic = code[6:code.find("DO")].replace(" ", "")
+            WhileCode = ""
+            temp = codeLines.pop(0)
+            while temp != "END":
+                WhileCode += temp + "\n"
+                temp = codeLines.pop(0)
             while parseLogic(logic):
-                runCode(WhileCode);
-            continue;
+                runCode(WhileCode)
+            continue
 
 # function that receives the string s and output value it corresponding to
 def parseLogic(s):
-    robotX = 0
-    robotY = 0
-    if s.contains("Destination"):
-        s.split("Destination");
+    if "Destination" in s:
+        s.split("Destination")
         return robotX != 3 & robotY != 3
-    elif s.contains(">"):
-        temp = s.split(">");
+    elif ">" in s:
+        temp = s.split(">")
         return parseValue(temp[0]) > parseValue(temp[1])
-    elif s.contains("<"):
-        temp = s.split("<");
+    elif "<" in s:
+        temp = s.split("<")
         return parseValue(temp[0]) < parseValue(temp[1])
-    elif s.contains("="):
+    elif "=" in s:
         temp = s.split("=")
         return parseValue(temp[0]) == parseValue(temp[1])
     else:
@@ -156,4 +157,4 @@ def moveRobot(code):
     print code
     return 0
 
-runCode(translateRFID("rfid.txt"))
+runCode(translateRFID("rfidIF.txt"))
