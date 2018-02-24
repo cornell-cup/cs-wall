dafeef4c1e50381eb8e3c9225712478827ecbc7c
EageRAssassin <rt389@cornell.edu>
2018-02-24 14:42:47 -0500

    GUI setup

--- GUI.py
+++ GUI.py
@@ -1,6 +1,21 @@
-import Tkinter
-root = Tkinter.Tk()
-for r in range(3):
-    for c in range(4):
-        Tkinter.Label(root, text='R%s/C%s' % (r, c), borderwidth=1 ).grid(row=r, column=c)
-root.mainloop()
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gallery(array, ncols=3):
+    nindex, height, width, intensity = array.shape
+    nrows = nindex//ncols
+    assert nindex == nrows*ncols
+    # want result.shape = (height*nrows, width*ncols, intensity)
+    result = (array.reshape(nrows, ncols, height, width, intensity)
+              .swapaxes(1,2)
+              .reshape(height*nrows, width*ncols, intensity))
+    return result
+
+def make_array():
+    import Image
+    return np.array([np.asarray(Image.open('face.png').convert('RGB'))]*12)
+
+array = make_array()
+result = gallery(array)
+plt.imshow(result)
+plt.show()
