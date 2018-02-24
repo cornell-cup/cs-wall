4f784efccc6074758bf2a19ae021bbf6b0ece9c6
EageRAssassin <rt389@cornell.edu>
2018-02-24 15:25:55 -0500

    move robot added

---
+++ moveRobot.py
@@ -0,0 +1,27 @@
+def moveRobot(code):
+    direction = 0
+    robotX = 0
+    robotY = 0
+    if code == "Forward":
+        if direction == 0:
+           robotY += 1
+        elif direction == 1:
+            robotX += 1;
+        elif direction == 2:
+          robotY -= 1;
+        elif direction == 3:
+            robotX -= 1
+    if code == "Backward":
+        if direction == 0:
+           robotY -= 1
+        elif direction == 1:
+            robotX -= 1;
+        elif direction == 2:
+          robotY += 1;
+        elif direction == 3:
+            robotX += 1
+
+    if code == "TurnLeft":
+        direction = (direction + 1) % 4
+    if code == "TurnRight":
+        direction = (direction + 3) % 4
