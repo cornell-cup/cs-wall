08945a9c25f6e3c8b597a4f7eab9bb73e0f57a0f
sophiezheng0711 <sophie.zheng@gmail.com>
2018-03-03 12:50:27 -0500

    updated the todo list

--- moveRobot.py
+++ moveRobot.py
@@ -2,8 +2,10 @@
 # TurnLeft, TurnRight and prints corresponding Minibot API
 # returns: <<<<SCRIPT,(code)>>>>, a String
 # Also note, I don't think that there are parentheses around the code according to their parser
-# Also, note that "dummy" will be changed
-# Question: do we need to move this on our grid? i.e. do we need the variables direction, robotX, and robotY?
+# TODO change "dummy" to actual power level
+# TODO "dummy" refers to the power needed for minibot to move 1 grid length OR turn
+# TODO figure out minibot's power level and whether we need to use the wait(time) function, also think about connecting
+# TODO robotX robotY to an imaginary gui
 
 
 def moveRobot(code):
@@ -11,6 +13,8 @@ def moveRobot(code):
     # direction = 0
     # robotX = 0
     # robotY = 0
+    move_lev = 50
+    turn_lev = 50
     if code == "Forward":
         # if direction == 0:
         #    robotY += 1
@@ -20,7 +24,7 @@ def moveRobot(code):
         #   robotY -= 1;
         # elif direction == 3:
         #     robotX -= 1
-        power = "dummy"
+        power = str(move_lev)
         s += "move_forward(" + power + ")\n"
     if code == "Backward":
         # if direction == 0:
@@ -31,15 +35,15 @@ def moveRobot(code):
         #   robotY += 1;
         # elif direction == 3:
         #     robotX += 1
-        power = "dummy"
+        power = str(move_lev)
         s += "move_backward(" + power + ")\n"
     if code == "TurnLeft":
         # direction = (direction + 1) % 4
-        power = "dummy"
+        power = str(turn_lev)
         s += "move_counter_clockwise(" + power + ")\n"
     if code == "TurnRight":
         # direction = (direction + 3) % 4
-        power = "dummy"
+        power = str(turn_lev)
         s += "move_clockwise(" + power + ")\n"
 
     s += ">>>>"
