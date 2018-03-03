0c4a274d8895da81ff40d451a98dc912d146d762
sophiezheng0711 <sophie.zheng@gmail.com>
2018-03-03 13:02:58 -0500

    changed move robot to a class

--- moveRobot.py
+++ moveRobot.py
@@ -8,43 +8,57 @@
 # TODO robotX robotY to an imaginary gui
 
 
-def moveRobot(code):
-    s = "<<<<SCRIPT,"
-    # direction = 0
-    # robotX = 0
-    # robotY = 0
-    move_lev = 50
-    turn_lev = 50
-    if code == "Forward":
-        # if direction == 0:
-        #    robotY += 1
-        # elif direction == 1:
-        #     robotX += 1;
-        # elif direction == 2:
-        #   robotY -= 1;
-        # elif direction == 3:
-        #     robotX -= 1
-        power = str(move_lev)
-        s += "move_forward(" + power + ")\n"
-    if code == "Backward":
-        # if direction == 0:
-        #    robotY -= 1
-        # elif direction == 1:
-        #     robotX -= 1;
-        # elif direction == 2:
-        #   robotY += 1;
-        # elif direction == 3:
-        #     robotX += 1
-        power = str(move_lev)
-        s += "move_backward(" + power + ")\n"
-    if code == "TurnLeft":
-        # direction = (direction + 1) % 4
-        power = str(turn_lev)
-        s += "move_counter_clockwise(" + power + ")\n"
-    if code == "TurnRight":
-        # direction = (direction + 3) % 4
-        power = str(turn_lev)
-        s += "move_clockwise(" + power + ")\n"
+class moveRobot:
 
-    s += ">>>>"
-    return s
+    global direction
+    direction = 1
+    global robot_x
+    robot_x = 3
+    global robot_y
+    robot_y = 1
+    global index
+    index = 0
+
+    def __init__(self):
+        print "hi"
+
+    def moveRobot(self, code):
+        s = "<<<<SCRIPT,"
+        global direction
+        global robotX
+        global robotY
+        move_lev = 50
+        turn_lev = 50
+        if code == "Forward":
+            if direction == 0:
+               robotY += 1
+            elif direction == 1:
+                robotX += 1;
+            elif direction == 2:
+              robotY -= 1;
+            elif direction == 3:
+                robotX -= 1
+            power = str(move_lev)
+            s += "move_forward(" + power + ")\n"
+        if code == "Backward":
+            if direction == 0:
+               robotY -= 1
+            elif direction == 1:
+                robotX -= 1;
+            elif direction == 2:
+              robotY += 1;
+            elif direction == 3:
+                robotX += 1
+            power = str(move_lev)
+            s += "move_backward(" + power + ")\n"
+        if code == "TurnLeft":
+            direction = (direction + 1) % 4
+            power = str(turn_lev)
+            s += "move_counter_clockwise(" + power + ")\n"
+        if code == "TurnRight":
+            direction = (direction + 3) % 4
+            power = str(turn_lev)
+            s += "move_clockwise(" + power + ")\n"
+
+        s += ">>>>"
+        return s
