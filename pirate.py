1dc2fc53b546141d5ad2ae13351a38259f4b28f0
EageRAssassin <rt389@cornell.edu>
2018-04-14 12:35:18 -0400

    path updated

--- pirate.py
+++ pirate.py
@@ -3,7 +3,7 @@ class Pirate:
     # the pirate's location in the grid in the form of [x, y]
     location = []
     # the pirate's path in the grid in the form of array of [x, y]
-    path = [][]
+    path = []
     # defines if the pirate is movable or not depending on the type of game
     movable = False
 
