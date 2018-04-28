d410a76a6721bef135e36d5fd13379630dc12396
sophiezheng0711 <sophie.zheng@gmail.com>
2018-04-28 12:38:17 -0400

    uncommented ece code

--- SystemControl.py
+++ SystemControl.py
@@ -1,6 +1,6 @@
 import time
 import Globals as G
-# import a4988
+import a4988
 
 
 class SystemControl:
@@ -54,7 +54,7 @@ class SystemControl:
             if check:
                 on_obstacle = True
                 return goal_reached, out, on_obstacle
-            # a4988.moveVerticalUp(10)
+            a4988.moveVerticalUp(10)
             print('moved')
         if code == "Backward":
             if self.direction == G.SOUTH:
